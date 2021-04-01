89a3932389d533b0892d269c28beb3a136b59a82
ksalic <kenan.salic@bloomreach.com>
2021-04-01 11:02:42 +0200

    env options updated

--- env-config.js
+++ env-config.js
@@ -1,6 +1,4 @@
 window._env_ = {
 BRX_BASE_URL: "https://kenan.bloomreach.io",
-BRX_USERNAME: "admin",
-BRX_PASSWORD: "admin",
 BRX_API_KEY: "fWzgxHFijsJQZEvaGsnbjwPzbNwSwgjwYVGhQwRfaMKignQxCHNYcYfxDMZorvuf",
 }
