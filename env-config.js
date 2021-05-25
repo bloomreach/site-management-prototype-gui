547a81c4545797877af5938234d95bd45d109960
Kenan Salic <kenan.salic@bloomreach.com>
2021-05-25 08:27:22 +0200

    update

--- env-config.js
+++ env-config.js
@@ -1,4 +1,4 @@
 window._env_ = {
-BRX_BASE_URL: "https://toolstation.bloomreach.io",
-BRX_API_KEY: "wnEjFZZDZJiCzAIoSzGQsVDmBtvAcWngSZMxiZrkBNJvmYPLsbUHxcaISGmPJIZt",
+BRX_BASE_URL: "https://profserv02.bloomreach.io",
+BRX_API_KEY: "qTNjxLeQqqarogkAjogRYvXRCkFuWqIqlcnNXXFJYQKxKjJTZmDqMRBuaMRXSJXy",
 }
